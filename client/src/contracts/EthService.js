e8a1beb1c326a9d65f9ee7024a1f2ee4874dbfa6
Danny Dalton <danny.dalton@trusttoken.com>
2020-05-28 12:24:32 -0700

    detecting metamask locked and metamask accounts changed

--- client/src/contracts/EthService.js
+++ client/src/contracts/EthService.js
@@ -4,10 +4,20 @@ const EthService = {
   state: {
     metamaskInstalled: false,
   },
-  init,
   web3Provider: null,
   accounts: null,
+  isMetamaskLocked,
+  init,
 };
+
+function isMetamaskLocked() {
+  console.log(window.web3.eth.accounts);
+  const accounts = window.web3.eth.accounts;
+  console.log('accounts length', accounts.length);
+  const isLocked = accounts.length === 0;
+  console.log('isLocked', isLocked);
+  return isLocked;
+}
 
 async function enableMetamask() {
   let res;
@@ -21,9 +31,16 @@ async function enableMetamask() {
   return res;
 }
 
+function handleMetamaskAccountsChangedEvent() {
+  window.ethereum.on('accountsChanged', function (accounts) {
+    console.log('metamask accounts changed coolio!');
+  });
+}
+
 async function init() {
   const ethereum = window.ethereum;
   if (typeof web3 !== 'undefined') {
+
     console.log('Metamask installed');
     EthService.state.metamaskInstalled = true;
 
@@ -32,19 +49,19 @@ async function init() {
     if (enableRes.code === 4001) {
         return false;
       } else {
+        handleMetamaskAccountsChangedEvent();
         EthService.accounts = enableRes;
         console.log(EthService.accounts);
+
+        EthService.web3Provider = new ethers.providers.Web3Provider(window.web3.currentProvider);
+        const signer = EthService.web3Provider.getSigner();
+        console.log(signer);
         // createTokenContracts();
         // console.log(web3);
         // web3.TUSDTokenContract.methods.balanceOf(web3State.accounts[0]).call().then((result) => {
         //   console.log(result);
         // });
       }
-
-    EthService.web3Provider = new ethers.providers.Web3Provider(window.web3.currentProvider);
-    const signer = EthService.web3Provider.getSigner();
-    console.log(signer);
-
   }
 }
 
