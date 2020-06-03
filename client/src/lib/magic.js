e68ae876374bb3c0735c94233d6f3f9ca70745cf
Danny Dalton <danny.dalton@trusttoken.com>
2020-06-02 18:12:06 -0700

    hopefully fix magic key env var

--- client/src/lib/magic.js
+++ client/src/lib/magic.js
@@ -1,5 +1,7 @@
 import { Magic } from 'magic-sdk';
 
+console.log('using key', process.env.MAGIC_PUBLISHABLE_KEY);
+
 const magic = new Magic(process.env.MAGIC_PUBLISHABLE_KEY, {
   network: "ropsten",
 });
