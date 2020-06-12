e52efaf357be4d4abef181886617716a0dce29cc
Danny Dalton <danny.dalton@trusttoken.com>
2020-06-12 14:39:02 -0700

    typo on magic link wallet create

--- client/src/lib/magic.js
+++ client/src/lib/magic.js
@@ -4,6 +4,7 @@ import { getEthNetwork } from '@/lib/eth';
 const network = getEthNetwork();
 
 console.log('we on network', network);
+console.log('magic p key', process.env.MAGIC_PUBLISHABLE_KEY);
 
 const magic = new Magic(process.env.MAGIC_PUBLISHABLE_KEY, {
   network,
