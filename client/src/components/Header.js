f4f91ec25d81a41abe1b52e8411d65a37fea0a6b
Piotr Kuchta <peter@kuchta.co.uk>
2020-06-22 22:49:50 +0200

    Work in progress

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,36 +3,70 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-      <div className="text-right" style={{ color: '#638298' }}>
+      <div>
         <small>
+          <div className="text-right" style={{ color: '#638298' }}>
+              <a
+                href="https://www.trusttoken.com/currencies"
+                target="_blank"
+                className="text-muted"
+              >
+                Products
+              </a>
 
-          <Link
-            to="/terms-of-use"
-            target="_blank"
-            className="text-muted"
-          >
-            Terms of Use
-          </Link>
+              <div className="mx-3 d-inline-block">|</div>
 
-          <div className="mx-3 d-inline-block">|</div>
+              <a
+                href="https://www.trusttoken.com/company"
+                target="_blank"
+                className="text-muted"
+              >
+                Company
+              </a>
 
-          <Link
-            to="/privacy-policy"
-            target="_blank"
-            className="text-muted"
-          >
-            Privacy Policy
-          </Link>
+              <div className="mx-3 d-inline-block">|</div>
 
-          <div className="mx-3 d-inline-block">|</div>
+              <a
+                className="text-muted"
+                target="_blank"
+                href="https://www.trusttoken.com/careers/"
+              >
+               Careers
+              </a>
 
-          <a
-            className="text-muted"
-            target="_blank"
-            href="mailto:support@trusttoken.com?subject=Ecosystem portal"
-          >
-           support@trusttoken.com
-          </a>
+              <div className="mx-3 d-inline-block">|</div>
+
+              <a
+                className="text-muted"
+                target="_blank"
+                href="https://app.trusttoken.com/signup-or-signin"
+              >
+               Get TrueUSD
+              </a>
+          </div>
+          <hr/>
+
+          <div className="text-right" style={{ color: '#638298' }}>
+
+              <Link
+                to="/privacy-policy"
+                target="_blank"
+                className="text-muted"
+              >
+                Privacy Policy
+              </Link>
+
+              <div className="mx-3 d-inline-block">|</div>
+
+              <Link
+                to="/terms-of-use"
+                target="_blank"
+                className="text-muted"
+              >
+                Terms of Use
+              </Link>
+
+          </div>
 
         </small>
       </div>
