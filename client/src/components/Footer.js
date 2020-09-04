aa963dc63273b0e169f7e2195a5c05dad1e5e6b2
Peter Kuchta <70281390+pkuchtatt@users.noreply.github.com>
2020-09-04 19:56:30 +0200

    Add links to forum (#35)
    * Add links to forum
    
    * Layout

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,17 +1,62 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Left = styled.span`
+  position: fixed;
+  left: 100px;
+  bottom: 0;
+  color: #1253FA;
+  text-align: left;
+  float: left;
+
+  .bluelink {
+    color: #1253FA;
+  }
+  .bluelink:hover {
+    color: #1253FA;
+  }
+`;
 
 const Footer = () => {
   return (
+    <>
       <div className="text-right" style={{
                                             position: 'fixed',
                                             left: 0,
                                             bottom: 0,
                                             width: '100%',
-                                            textAalign: 'right',
+                                            textAlign: 'right',
                                         }}
       >
+
+        <Left>
+          <small>
+            <Link
+              to={{ pathname: "https://forum.trusttoken.com" }}
+              target="_blank"
+              className="bluelink"
+            >
+              Forum
+            </Link>
+            <div className="mx-3 d-inline-block">&nbsp;</div>
+
+            <Link
+              to={{ pathname: "https://forum.trusttoken.com/t/claim-tru-using-trusttoken-purchaser-portal-live/1009/2" }}
+              target="_blank"
+              className="bluelink"
+            >
+              How it works
+            </Link>
+
+            <div className="mx-3 d-inline-block">&nbsp;</div>
+
+          </small>
+        </Left>
+
+
         <small>
+
 
           <Link
             to="/terms-of-use"
@@ -43,6 +88,7 @@ const Footer = () => {
 
         </small>
       </div>
+</>
   )
 }; 
 
